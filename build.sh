b0d8012453a88528cc0892a416b647b0878870e5
enp0s10 <cdixon99@ivytech.edu>
2021-02-03 14:36:08 -0500

    Better error
    not to be a boomer but back in my day we had better humor

--- build.sh
+++ build.sh
@@ -1,89 +1,22 @@
-#!/bin/bash
-
-echo "
-// Useful headers
-#include <cstdlib>
-#include <cstdio>
-#include <ctime>
-#include <cmath>
-#include <climits>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <numeric>
-#include <string>
-#include <regex>
-#include <limits>
-#include <vector>
-#include <functional>
-#include <stdexcept>
-
-// Useful declarations
-using std::stringstream;
-using std::ostringstream;
-using std::regex_match;
-using std::regex;
-using std::ofstream;
-using std::ifstream;
-using std::isdigit;
-using std::string;
-using std::vector;
-using std::cerr;
-using std::system;
-using std::string;
-using std::to_string;
-
-// Def Macro
-#define def auto
-
-// Proc Macro
-#define proc(X) = [](X)
-
-// Loop Macro
-#define loop(X, Y) for(int X = 0; X < Y; X++)
-
-// Input Macro
-#define input(X) std::cout << \"> \"; std::getline(std::cin, X)
-
-// Print Macro
-#define print(X) std::cout << X << std::endl
-
-// Stop Macro
-#define stop return 0
-
-// Seed Macro
-#define seed srand((int)time(0))
-
-// Random function
-int random(int arg) {
-  int result = 0;
-  if (arg > 1) {
-    result = (rand() % arg) + 1;    
   }
   return result;
 }
-
 // System calls
 void systemCall(string arg) {
   const char * c = arg.c_str();
   system(c);
 }
-
 // Shell Macro
 #define shell(X) systemCall(X)
-
 // Error handling Macro
 #define handle(X) catch (std::invalid_argument& X)
-
 // Type conversion
 int to_int(string arg) {
   return std::stoi(arg);
 }
-
 int to_double(string arg) {
   return std::stod(arg);
 }
-
 // Numeric strings
 bool is_int(string arg) {
   bool value = true;
@@ -95,7 +28,6 @@ bool is_int(string arg) {
   }
   return value;
 }
-
 bool is_double(string arg) {
   bool value = true;
   try {
@@ -106,11 +38,9 @@ bool is_double(string arg) {
   }
   return value;
 }
-
 bool is_bool(string arg) {
   return (arg == \"0\" || arg == \"1\");
 }
-
 int main() {
 " >> main.cpp
 
@@ -123,16 +53,3 @@ then
 fi
 
 g++ main.cpp -flto -O -std=c++20 &>/dev/null
-
-if test -f "a.out"
-then
-  echo
-  echo "Well done, Boomer! Time to run your binary?"
-  echo
-  rm main.cpp
-else
-  echo
-  echo "You made a mistake there Boomer. Edit your code and try again?"
-  echo
-  rm main.cpp
-fi
